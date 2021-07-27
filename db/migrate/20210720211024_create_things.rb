48dbf005adbea94c6b82ede5d316e763b4e5216d
Maud de Vries <emcoding@pm.me>
2021-07-27 18:36:21 +0200

    Init for use with TailwindUi
    By adding some data and a root route

--- db/migrate/20210720211024_create_things.rb
+++ db/migrate/20210720211024_create_things.rb
@@ -1,6 +1,11 @@
 class CreateThings < ActiveRecord::Migration[6.1]
   def change
     create_table :things do |t|
+      t.boolean :admin
+      t.boolean :tested
+      t.string :title
+      t.text :description
+      t.integer :score
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# t.boolean :admin
+# t.boolean :tested
+# t.string :title
+# t.text :description
+# t.integer :score
+
+3.times do |i|
+  Thing.create!(admin: [true,false, false, false, false, false].sample,
+                tested: [true, true, true, false, true, false].sample,
+                title: "Say it #{i} more times",
+                description: "#{i} is the description",
+                score: [1..10].sample
+  )
+end
+
