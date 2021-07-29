2790e738584f6ef9bc157e727bf855e744cec5df
Maud de Vries <emcoding@pm.me>
2021-07-29 13:50:53 +0200

    Add additional plugins for forms and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
     variants: {
       extend: {},
     },
-    plugins: [],
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+    ],
   },
 }
