88116f859198b4e108d28d203c438f00c258f596
Maud de Vries <emcoding@pm.me>
2021-08-01 11:00:19 +0200

    :card_index: Move to pg for Heroku; using magic rails
    `rails db:system:change --to=postgresql` :hearteyes:

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_20_211024) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "things", force: :cascade do |t|
     t.boolean "admin"
     t.boolean "tested"
